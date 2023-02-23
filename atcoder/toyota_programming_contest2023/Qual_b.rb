d673fd4459c51ffba036d51f3e66e4abd3e0cb7c
hisaki <alltimelow2011@gmail.com>
2023-02-24 08:53:51 +0900

    add: another answer of qual_b

--- atcoder/toyota_programming_contest2023/Qual_b.rb
+++ atcoder/toyota_programming_contest2023/Qual_b.rb
@@ -14,3 +14,17 @@ s.each do |i|
 end
 
 puts s_res.join
+
+# n,k=gets.chomp.split(" ").map(&:to_i)
+# s=gets.chomp.split("")
+# ct=0
+# ans=""
+# n.times do |i|
+#     if s[i]=="o" && ct<k
+#         ans+="o"
+#         ct+=1
+#     else
+#         ans+="x"
+#     end
+# end
+# puts ans
