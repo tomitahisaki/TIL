12fc500fabbae228e9ef2f1357a935ba4668eb06
hisaki <alltimelow2011@gmail.com>
2023-07-20 06:27:41 +0900

    money gem is installed and done

--- ruby/gold/meta_programing/second/monetize_example.rb
+++ ruby/gold/meta_programing/second/monetize_example.rb
@@ -1,4 +1,7 @@
-require 'Monetize'
+require_relative 'config/initializers/i18n'
+require_relative 'config/initializers/money'
+require "money"
 
-price = Money.new(100_00, "USD")
-puts price
+currency = Money.from_cents(1000, "USD").currency
+p currency.iso_code #=> "USD"
+p currency.name   #=> "United States Dollar"
