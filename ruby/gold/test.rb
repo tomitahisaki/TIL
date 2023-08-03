8176ec0d0d31a1e6ae65b0dbe0204786b4746643
hisaki <alltimelow2011@gmail.com>
2023-08-04 06:11:48 +0900

    class_eval what is difference between block and context

--- ruby/gold/test.rb
+++ ruby/gold/test.rb
@@ -1,12 +1,20 @@
-local = 0
+class Foo; end
 
-p1 = Proc.new { |arg1, arg2|
-  arg1, arg2 = arg1.to_i, arg2.to_i
-  local += [arg1, arg2].max
-}
+# ネストが変化しない
+Foo.class_eval do
+  CONST_IN_BLOCK = 100
+end
 
-p1.call("1", "2")
-p1.call("7", "5")
-p1.call("9")
+# ネストが変化する
+Foo.class_eval(<<-EVAL)
+  CONST_IN_HERE_DOC = 200
+EVAL
 
-p local
+puts "BLOCK: CONST is defined? #{Foo.const_defined?(:CONST_IN_BLOCK, false)}"
+puts "BLOCK: CONST is defined? #{Object.const_defined?(:CONST_IN_BLOCK, false)}"
+
+puts "HERE_DOC: CONST is defined? #{Foo.const_defined?(:CONST_IN_HERE_DOC, false)}"
+puts "HERE_DOC: CONST is defined? #{Object.const_defined?(:CONST_IN_HERE_DOC, false)}"
+
+p CONST_IN_BLOCK
+p Foo::CONST_IN_HERE_DOC
