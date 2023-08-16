dda2c6894e2ccb98b15f5069098c5cdb37d13582
hisaki <alltimelow2011@gmail.com>
2023-08-16 16:16:04 +0900

    add css and yarn build && yarn start

--- Typescript/random-cat/pages/index.tsx
+++ Typescript/random-cat/pages/index.tsx
@@ -1,32 +1,50 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
- 
-const IndexPage: NextPage = () => {
-  // ❶ useStateを使って状態を定義する
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
-  // ❷ マウント時に画像を読み込む宣言
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url); // 画像URLの状態を更新する
-      setLoading(false); // ローディング状態を更新する
-    });
-  }, []);
-  // ボタンをクリックしたときに画像を読み込む処理
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
   const handleClick = async () => {
-    setLoading(true); // 読込中フラグを立てる
+    setLoading(true);
     const newImage = await fetchImage();
-    setImageUrl(newImage.url); // 画像URLの状態を更新する
-    setLoading(false); // 読込中フラグを倒す
+    setImageUrl(newImage.url);
+    setLoading(false);
   };
   return (
-    <div>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
-      <div>{loading || <img src={imageUrl} />}</div>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
     </div>
   );
 };
 export default IndexPage;
+ 
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
  
 type Image = {
   url: string;
@@ -37,7 +55,3 @@ const fetchImage = async (): Promise<Image> => {
   console.log(images);
   return images[0];
 };
-
-fetchImage().then((image) => {
-  console.log(image.alt);
-});
