c21ad94e06f921b4a848374dfb80d89f6d5ca4f7
hisakey <alltimelow@gmail.com>
2024-10-15 09:15:37 +0900

    algo practice tmp

--- algo_practice/atcoder/tessoku_book/a08.rb
+++ algo_practice/atcoder/tessoku_book/a08.rb
@@ -1,16 +1,17 @@
-h, w = gets.split.map(&:to_i)
-cell = []
-targets = []
-
-h.times do |i|
-  cell[i] = gets.split.map(&:to_i)
+height, width = gets.split.map(&:to_i)
+row = []
+height.times do
+  row << gets.chomp.split(&:to_i)
 end
 
-n = gets.to_i
-n.times do |i|
-  targets[i] = gets.split.map(&:to_i)
+cumulative_area = []
+height.times do |i|
+  previous_num = 0
+  cumulative_row = []
+  width.times do |j|
+    add_num = row[i][j] + previous_num
+    cumulative_row << add_num
+    previous_num = add_num
+  end
+  cumulative_area << cumulative_row
 end
-
-
-
-sums
