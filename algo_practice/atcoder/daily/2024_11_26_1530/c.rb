48a2c08bb843f831f0c47cb72deb15b63c09ffd1
hisakey <alltimelow2011@gmail.com>
2024-11-28 06:46:51 +0900

    feat: algo practice

--- algo_practice/atcoder/daily/2024_11_26_1530/c.rb
+++ algo_practice/atcoder/daily/2024_11_26_1530/c.rb
@@ -1,4 +1,13 @@
 s = gets.chomp
 t = gets.chomp
 
-puts 'Yes' if s == t
+result = []
+chars = s.chars
+
+(chars.size - 1).times do |i|
+  swapped_chars = chars.dup
+  swapped_chars[i], swapped_chars[i + 1] = swapped_chars[i + 1], swapped_chars[i]
+  result << swapped_chars.join
+end
+result << chars.join
+puts result.include?(t) ? 'Yes' : 'No'
